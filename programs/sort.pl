12b8f9f7b950ea5e457ac1e2949b1e835697858b
Andrzej Górski <andrzejgorski@supermond.com>
2015-10-13 15:54:47 +0200

    Dodano materiał z labów

--- programs/sort.pl
+++ programs/sort.pl
@@ -1,15 +1,26 @@
-lessOrEq(0, X).
-lessOrEq(s(X), s(Y)):- lessOrEq(X, Y).
 
 sort(X, Y):- perm(X, Y), sorted(Y).
+
 sorted([]).
 sorted([X|L1]):-
     lessThenHead(X, L1),
     sorted(L1).
 
-lessThenHead(X, []).
-lessThenHead(X, [Y, L]):-
-    less(X, Y).
+lessThenHead(_, []).
+lessThenHead(X, [Y| _L]):-
+    lessOrEq(X, Y).
 
-% TODO
-perm(X, Y):- 
+lessOrEq(X, Y):- X =< Y.
+
+perm(X, Y):-
+    zawiera(X, Y),
+    zawiera(Y, X).
+
+zawiera([], _List).
+zawiera([El| Lista1], Lista2):-
+    usun(El, Lista2, Lista3),
+    zawiera(Lista1, Lista3).
+
+usun(_Element, _Wynik, [], _Wynik).
+usun(_Element, [_Element| Lista], _Acumulator, Lista).
+usun(_Element, [_Element | _Lista1], _Lista2).
